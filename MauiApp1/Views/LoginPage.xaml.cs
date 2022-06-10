a29fd21b9b296efcd6985b58c0f4cb201b1fe642
Jim Zhao <jzhao@geospatialtech.com>
2022-06-10 12:07:19 -0700

    barebones

--- MauiApp1/Views/LoginPage.xaml.cs
+++ MauiApp1/Views/LoginPage.xaml.cs
@@ -1,12 +1,9 @@
-using MauiApp1.ViewModels;
-
 namespace MauiApp1.Views;
 
 public partial class LoginPage : ContentPage
 {
 	public LoginPage()
 	{
-		BindingContext = new LoginViewModel();
-		InitializeComponent();
+        InitializeComponent();
 	}
 }
